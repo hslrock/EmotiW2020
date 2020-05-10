32754a6f6e140b8aeedb0785b338e0b922df6e9d
Hyun Seung Lee <hslrock@gmail.com>
2020-05-10 11:36:29 +0900

    update

--- Dataload/dataload.py
+++ Dataload/dataload.py
@@ -4,10 +4,12 @@ import sys
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
-#from src import audio_dataset
+from src import detect_faces, show_bboxes
 from PIL import Image
 import torch
+np_load_old = np.load
 
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
 class pre_train_data(Dataset):
     def __init__(self, csv_file,path):
         self._table=pd.read_csv(csv_file)
@@ -37,70 +39,135 @@ class Video_Embedding_Data(Dataset):
 
     def __getitem__(self, idx):
         embedding=torch.from_numpy(np.array(self._table_embedding.Embedding[idx].split(),dtype=float)).reshape((self.frame,-1))
-        labels = torch.from_numpy(np.array(self._table_label.Label[idx]))-1
         
-        return (embedding,labels.long())
+        labels = torch.from_numpy(np.array(self._table_label.Label[idx]))-1
+        empty=0
+        return (embedding,empty,labels.long())
+    
     
 class Video_Frame_Data(Dataset):
-    def __init__(self, csv_file,base_path_v=None,base_path_a=None,strict_num=16,strict_name=True,name_format=9,transform=None):
+    def __init__(self,csv_file,sub_csv_file=None,
+                 base_path_v=None,base_path_a=None,frame_num=16,strict_name=True,name_format=9,embedding=False):
+        
+        self.max_frame_num=24
         self._table = pd.read_csv(csv_file,delimiter=' ')
-        self.strict_num = strict_num
+        if sub_csv_file is None:
+            self._table_embedding=None
+        else:
+            self._table_embedding=pd.read_csv(sub_csv_file)
+        self.frame_num = frame_num
         self._base_path_v=base_path_v
         self._base_path_a=base_path_a
+        self.embedding=embedding
         if strict_name:
             self.name_format=name_format
-
+            
 
         self.transform=transforms.Compose([
                      transforms.Resize((256,256)),
-                     #transforms.Grayscale(),
                      transforms.ToTensor(),   
-                     #transforms.Normalize((0.5, ), (0.5, ))])
                      transforms.Normalize((0.5,0.5,0.5 ), (0.5, 0.5,0.5))])
-       # print(self._table.set_index(["Attribute","Wav_Path"]).count(level="Attribute"))
     def __len__(self):
         return len(self._table)
 
     def __getitem__(self, idx):
         folder_name = os.path.join(self._base_path_v,self._table.Vid_name[idx])
         first=True
-        
-        #audio_img=audio_dataset.plotstft(os.path.join(self._base_path_a,self._table.Attribute[idx],
-        #                                    str(self._table.file_path[idx]).zfill(self.name_format)+'.wav'))
         audio_img=np.zeros((1,1))
-        
-        
-        ###Read Frames
-        START_PAD = Image.new(mode = "RGB", size = (256, 256), color = (255, 255, 255) )
-        END_PAD= Image.new(mode = "RGB", size = (256, 256), color =(0, 0, 0))
-        
-        img=self.transform(START_PAD).unsqueeze(0)
-        END_img=self.transform(END_PAD).unsqueeze(0)
-        frame_len=len(os.listdir(folder_name))
-        
-        if frame_len<self.strict_num-2:
-            for index_0 in range(frame_len):
-                tempimg=Image.open(os.path.join(folder_name,os.listdir(folder_name)[index_0]))#.convert('L')       
-                tempimg=self.transform(tempimg)
-                tempimg=tempimg.unsqueeze(0)
-                img=torch.cat((img,tempimg))
-            for index_1 in range(self.strict_num-2-frame_len):
-                img=torch.cat((img,END_img))
-                
-        else:    
-            frame_index=(np.linspace(0,frame_len-1,self.strict_num-2,dtype=int))
-
-            for index_2 in frame_index:
-                tempimg=Image.open(os.path.join(folder_name,os.listdir(folder_name)[index_2]))#.convert('L')
-                tempimg=self.transform(tempimg)
-                tempimg=tempimg.unsqueeze(0)
-                img=torch.cat((img,tempimg))
-
         labels = torch.from_numpy(np.array(self._table.Label[idx]))-1
             
-     
-            
-        img=torch.cat((img,END_img))
-        img=img[1: -1]
-        return (img,audio_img,labels)
+        if self._table_embedding is not None:
+            temp_frame_embedding=torch.from_numpy(np.array(self._table_embedding.Embedding[idx].split(),dtype=float)).reshape((self.max_frame_num,-1))
+            frame_data=torch.empty(size=(self.frame_num,1000),dtype=torch.double)
+            if self.frame_num<25:
+                index=np.linspace(0,23,self.frame_num,dtype=int)
+ 
+                for i,copy in enumerate(index):
+                    frame_data[i]=temp_frame_embedding[copy]
+            return (frame_data,audio_img,labels)
     
+
+            
+            
+        frame_len=len(os.listdir(folder_name))
+        frame_list=[]
+        if frame_len<self.frame_num:
+            for index_0 in range(frame_len):
+                frame_path=os.path.join(folder_name,os.listdir(folder_name)[index_0])
+                tempimg=Image.open(frame_path)       
+               # frame_class=frame(tempimg,frame_path)
+                frame_list.append(self.transform(tempimg))
+        else:    
+            frame_index=(np.linspace(0,frame_len-1,self.frame_num,dtype=int))
+
+            for index_2 in frame_index:
+                frame_path=os.path.join(folder_name,os.listdir(folder_name)[index_2])
+                tempimg=Image.open(frame_path)
+                #frame_class=frame(tempimg,frame_path)
+                frame_list.append(self.transform(tempimg))
+        frame_data=torch.stack(frame_list,dim=0)
+
+        
+
+
+
+                
+                
+            
+        return (frame_data,audio_img,labels)
+    
+class multi_frames():
+    def __init__(self,frame_list,channel=3,img_width=256,img_length=256,embed_dim=1000,frame_embedding=None):
+        self.frame_list=frame_list
+        
+        if frame_embedding is None: 
+            self.frames = torch.empty(size=(len(frame_list), channel, img_width,img_length)) 
+            for index,element in enumerate(frame_list):
+                self.frames[index]=element.frame_img
+        else:
+            self.frames=frame_embedding
+    def __len__(self):
+        return len(self.frame_list)
+
+        
+
+class frame():
+    def __init__(self,frame_img,frame_name,max_face=5):
+        self.frame_img=frame_img
+        self._file_name=frame_name
+        self.transform_face=transforms.Compose([
+                                 transforms.Resize((32,32)), 
+                                 transforms.ToTensor(),
+                                 transforms.Normalize((0.5,0.5,0.5 ), (0.5, 0.5,0.5))])
+        
+        self.transform_frame=transforms.Compose([
+                     transforms.Resize((256,256)),
+                     transforms.ToTensor(),   
+                     transforms.Normalize((0.5,0.5,0.5 ), (0.5, 0.5,0.5))])
+
+        
+        def face_extraction(img,max_face):
+            bounding_boxes, landmarks = detect_faces(img)
+            img_list=[]
+            for box_index,(left,right,up,bottom,_) in enumerate(bounding_boxes):
+                cropped_img=img.crop((left,right,up,bottom))
+                img_list.append(self.transform_face(cropped_img))
+                if len(img_list)==max_face:
+                    break
+            while len(img_list) !=max_face:
+                END_PAD= Image.new(mode = "RGB", size = (256, 256), color =(0, 0, 0))
+                img_list.append(self.transform_face(END_PAD))
+            img_list=torch.stack(img_list)
+
+            return img_list
+        self.faces=face_extraction(self.frame_img,max_face)
+
+
+        
+        self.frame_img=self.transform_frame(frame_img)
+
+        
+        
+    
+        
+        
--- module/video_model.py
+++ module/video_model.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from module import transformer,densenet
+from src import detect_faces, show_bboxes
 
 class Video_Feature(nn.Module):
     
@@ -15,7 +16,8 @@ class Video_Feature(nn.Module):
 
         #self.resnet_1=resnet.resnet18(dim=dim,grayscale=grayscale)
         self.densenet=densenet.densenet121(pretrained=True)
-
+        self.dim_frame=1000
+        self.dim_face=100
         #self.fc1=nn.Linear(dim,1024)
         self.fc2=nn.Linear(1000,512)
         self.fc3=nn.Linear(512, self.label)
@@ -29,7 +31,12 @@ class Video_Feature(nn.Module):
         x1 = x1.view(x1.size(0), -1)
         
         return x1
+    def check_raw(self,x,dim):
+        if x.shape[-1]==dim:
+            return False
+        return True 
         
+        return None
     def stack_frame(self,x):
         ##Transformation to: Frames*Channel*width*height
 
@@ -44,80 +51,9 @@ class Video_Feature(nn.Module):
         return cnn_embed_seq
         
     def forward(self,x):
-        if not self.skip_process:
-            cnn_embed_seq=self.stack_frame(x)
-        
-            cnn_embed_seq=self.posencoding(cnn_embed_seq) 
-           # return cnn_embed_seq
-        else:
-            cnn_embed_seq=x
-        ##Input to Transformer
-        
-        cnn_embed_seq=self.fc2(cnn_embed_seq)
-        output=self.transformer(cnn_embed_seq,cnn_embed_seq,cnn_embed_seq)
-        output,attention=output[0],output[1]
-        #print(attention.shape)
-       # output=self.fc2(F.relu((self.fc1(output.transpose(1,2)))))
-        output=(output.transpose(1,2))
-        if self.joint==False:
-            output=self.fc3(F.relu(output))
-        
-        
-        if self.classfication:
-            output=F.relu(output)
-            output=output.squeeze(1)
-            if self.joint==True:
-                return output
-            else:
-                return F.softmax(output, 1)
-        else:
-            return output.squeeze(1)
-
-        
-class Video_Feature_1(nn.Module):
-    
-    def __init__(self,frame=24,dim=1000,joint=True,grayscale=True,classfication=True,skip_process=True):
-        super(Video_Feature, self).__init__()
-        self.label=1
-        self.classfication=classfication
-        self.skip_process=skip_process
-        if classfication:
-            self.label=3
-
-        #self.resnet_1=resnet.resnet18(dim=dim,grayscale=grayscale)
-        self.densenet=densenet.densenet121(pretrained=True)
-
-        #self.fc1=nn.Linear(dim,1024)
-        self.fc2=nn.Linear(1000,512)
-        self.fc3=nn.Linear(512, self.label)
-        self.joint=joint
-        self.posencoding=transformer.PositionalEncoding(1000,n_position=frame)
-        
-        self.transformer=transformer.MultiHeadAttention(d_k=256,d_v=256,n_head=2,frame=frame)
-        
-    def frame_embedder(self,x,t):
-        x1 = (self.densenet(x[:, t, :, :, :]))  # Pretrained_Densenet
-        x1 = x1.view(x1.size(0), -1)
-        
-        return x1
-        
-    def stack_frame(self,x):
-        ##Transformation to: Frames*Channel*width*height
-
-        cnn_embed_seq = []
-     
-        for t in range(x.size(1)):
-            with torch.no_grad():
-                x1=self.frame_embedder(x,t)
-                cnn_embed_seq.append(x1)            
-        cnn_embed_seq = torch.stack(cnn_embed_seq, dim=0).transpose_(0, 1)
-        
-        return cnn_embed_seq
-        
-    def forward(self,x):
-        if not self.skip_process:
-            cnn_embed_seq=self.stack_frame(x)
-        
+        if  self.check_raw(x,self.dim_frame):
+            print("hi")
+            cnn_embed_seq=self.stack_frame(x)  
             cnn_embed_seq=self.posencoding(cnn_embed_seq) 
            # return cnn_embed_seq
         else:
